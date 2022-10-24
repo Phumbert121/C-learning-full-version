e827f1a04b7e85cae90d2d94b1e11ae3bc76814f
Igor <igor.zorin121@gmail.com>
2022-10-24 22:13:11 +0400

    Added homeworks from 1 to 7

--- Лекция 6/ДЗ6/Program.cs
+++ Лекция 6/ДЗ6/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 41
+// Console.WriteLine("Введите количество чисел М в массиве: ");
+// int M = Convert.ToInt32(Console.ReadLine());
+
+// int[] array = new int[M];
+// int index = 0;
+
+// Console.WriteLine("А теперь введите числа массива в количестве М: ");
+// while (index < M)
+// {
+//     array[index] = Convert.ToInt32(Console.ReadLine());
+//     index++;
+// }
+// index = 0;
+// int count = 0;
+
+// while (index < M)
+// {
+//     if (array[index] > 0)
+//         count++;
+//     index++;
+// }
+
+// Console.WriteLine("Количество чисел массива больше 0: " + count);
+
+// Задача 43
+
+// Console.WriteLine("Введите числа k1, b1, k2, b2: ");
+// double k1 = Convert.ToInt32(Console.ReadLine());
+// double b1 = Convert.ToInt32(Console.ReadLine());
+// double k2 = Convert.ToInt32(Console.ReadLine());
+// double b2 = Convert.ToInt32(Console.ReadLine());
+
+// double x = (b1 - b2)/(k2 - k1);
+// double y = k2 * x + b2;
+
+// Console.WriteLine("Координаты точки пересечения прямых: (" + x + "; " + y + ")");
---
+++ Лекция 7/ДЗ7/Program.cs
@@ -0,0 +1,72 @@
+﻿// // Задача 47
+
+// Console.WriteLine("Введите количество строк и столбцов m и n: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// double[,]matrix = new double[m, n];
+
+// for (int i = 0; i < m; i++)
+// {
+//     for (int j = 0; j < n; j++)
+//     {
+//         matrix[i, j] = Math.Round(new Random().NextDouble() * 100, 2);
+//     }
+// }
+
+// for (int i = 0; i < m; i++)
+// {
+//     for (int j = 0; j < n; j++)
+//     {
+//         Console.Write(matrix[i, j] + " ");
+//     }
+//     Console.WriteLine();
+// }
+
+// // Задача 50 (запускать совместно с предыдущей задачей)
+
+// Console.WriteLine("Введите позиции элемента массива l(строки) и k(столбца): ");
+// int l = Convert.ToInt32(Console.ReadLine());
+// int k = Convert.ToInt32(Console.ReadLine());
+
+// if (l >= m || k >= n)
+// {
+//     Console.WriteLine("Такого числа в массиве нет.");
+//     return;
+// }
+// Console.WriteLine("Вот этот элемент: " + matrix[l, k]);
+
+// Задача 52
+
+// Console.WriteLine("Введите количество строк и столбцов m и n: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// int n = Convert.ToInt32(Console.ReadLine());
+// int[,]matrix = new int[m, n];
+// double average = 0;
+
+// for (int i = 0; i < m; i++)
+// {
+//     for (int j = 0; j < n; j++)
+//     {
+//         matrix[i, j] = new Random().Next(0, 100);
+//     }
+// }
+
+// for (int i = 0; i < m; i++)
+// {
+//     for (int j = 0; j < n; j++)
+//     {
+//         Console.Write(matrix[i, j] + " ");
+//     }
+//     Console.WriteLine();
+// }
+
+// for (int j = 0; j < n; j++)
+// {
+//     for (int i = 0; i < m; i++)
+//     {
+//         average += matrix[i, j];
+//     }
+//     Console.WriteLine("Среднее арифметическое " + j + "-го столбца: " + average / m);
+//     average = 0;
+// }
